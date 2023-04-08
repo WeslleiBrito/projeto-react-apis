a891c1c53020ac5d51cc7155b98089a008656514
WeslleiBrito <wesllei.wbs@gmail.com>
2023-04-08 11:55:39 -0300

    Criando a funcao reaplaceText, incio

--- pokedex/src/App.js
+++ pokedex/src/App.js
@@ -7,11 +7,34 @@ function App() {
 
   const [pokemons, setPokemons] = useState([])
 
+  const replaceText = (text, firstUpperCase, space) => {
+    const regex = /[a-z]/i
+    const textArray = text.split("")
+    let uppercase = false
+
+    const textClean = textArray.map((caracter, index) => {
+
+
+      if (regex.test(caracter)) {
+
+        const letter = (uppercase || index === 0) & firstUpperCase ? caracter.toUpperCase() : caracter
+        uppercase = false
+        return letter
+      }
+
+      uppercase = true
+      return space ? " " : ""
+
+    })
+
+    return textClean.toString().replaceAll(",", "")
+  }
+
   const getPokemon = async () => {
     try {
       const result = (await axios(`${URL_BASE}pokemon?offset=0&limit=40`)).data.results;
       const pokemonUrls = result.map((item) => item.url);
-      
+
       // Nessa etapa ele retorna uma lista com as promessas de que ele vai ter um resultado daquelas requisições
       const pokemonPromises = pokemonUrls.map(async (url) => {
         try {
@@ -21,29 +44,30 @@ function App() {
           console.log(error.response);
         }
       });
-   /* Nessa etapa a função promise.all aguarda que todas as promessas sejam resulvida
-    e ai sim retorna a lista com os resultados, tanto positivo, como negativo*/
+      /* Nessa etapa a função promise.all aguarda que todas as promessas sejam resulvida
+       e ai sim retorna a lista com os resultados, tanto positivo, como negativo*/
 
       const pokemonResults = await Promise.all(pokemonPromises);
 
       const pokemonsClear = pokemonResults.map((item) => {
         return {
-          id: item.id, 
-          name: item.name,
-          types: item.types.map((itemTypes) => {return itemTypes.type.name}),
+          id: item.id,
+          name: replaceText(item.name, true),
+          types: item.types.map((itemTypes) => { return itemTypes.type.name }),
           imagePokemonDefault: item.sprites.other.dream_world.front_default,
           secondImagesPokemons: {
             frontMale: item.sprites.front_default,
             frontFemale: item.sprites.front_female,
             backMale: item.sprites.back_default,
             backFemale: item.sprites.back_female
-          }, 
-          moves: item.moves.slice(0, 10).map((itemMove) => {return itemMove.move.name}),
+          },
+          moves: item.moves.slice(0, 10).map((itemMove) => { return replaceText(itemMove.move.name, true, true) }),
           baseStats: item.stats.map((statItem) => {
-            return {[statItem.stat.name]: statItem.base_stat}
+            return { [replaceText(statItem.stat.name)]: statItem.base_stat }
           })
         }
       })
+
       setPokemons(pokemonsClear);
     } catch (error) {
       console.log(error.response);
@@ -60,9 +84,9 @@ function App() {
 
 
   return (
-   <>
+    <>
 
-   </>
+    </>
   );
 }
 
