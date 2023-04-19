4a6aa83215797ada7661fc667b328c40f429ec75
WeslleiBrito <wesllei.wbs@gmail.com>
2023-04-19 12:30:29 -0300

    Hook de buscar pokemon/pokemons finalizado

--- pokedex/src/App.js
+++ pokedex/src/App.js
@@ -1,7 +1,5 @@
-import axios from "axios";
 import { useState, useEffect } from "react";
 import { GlobalStyle } from "./globalStyled";
-import { URL_BASE } from "./constants/URL_BASE";
 import { Router } from "./Routes/Router";
 import { Header } from "./components/Header/Header";
 import { useFechtPokemons } from "./hooks/useFetchPokemons";
@@ -29,60 +27,17 @@ export const replaceText = (text, firstUpperCase, space) => {
   return textClean.toString().replaceAll(",", "")
 }
 
+
 function App() {
 
-  const [pokemons, setPokemons] = useState([])
+  const {pokemons} = useFechtPokemons()
   const [page, setPage] = useState({
     home: false,
     pokeball: true,
     details: false
   })
 
-  const getPokemon = async () => {
-    try {
-      const result = (await axios(`${URL_BASE}pokemon?offset=0&limit=21`)).data.results
-      const pokemonUrls = result.map((item) => item.url)
 
-      // Nessa etapa ele retorna uma lista com as promessas de que ele vai ter um resultado daquelas requisições
-      const pokemonPromises = pokemonUrls.map(async (url) => {
-        try {
-          const pokemon = await axios(url)
-          return pokemon.data
-        } catch (error) {
-          console.log(error.response)
-        }
-      });
-      /* Nessa etapa a função promise.all aguarda que todas as promessas sejam resulvida
-       e ai sim retorna a lista com os resultados, tanto positivo, como negativo*/
-
-      const pokemonResults = await Promise.all(pokemonPromises)
-
-      const pokemonsClear = pokemonResults.map((item) => {
-        return {
-          id: item.id,
-          name: replaceText(item.name, true),
-          types: item.types.map((itemTypes) => { return itemTypes.type.name }),
-          imagePokemonDefault: item.sprites.other.dream_world.front_default,
-          secondImagesPokemons: [item.sprites.front_default, item.sprites.back_default, item.sprites.front_female, item.sprites.back_female],
-          moves: item.moves.slice(0, 10).map((itemMove) => { return replaceText(itemMove.move.name, true, true) }),
-          baseStats: item.stats.map((statItem) => {
-            return { [replaceText(statItem.stat.name)]: statItem.base_stat }
-          })
-        }
-      })
-
-      setPokemons(pokemonsClear)
-
-    } catch (error) {
-      console.log(error.response)
-    }
-  };
-
-  useEffect(() => {
-    getPokemon()
-  }, [])
-
-  console.log(useFechtPokemons())
   return (
     <>
       <GlobalStyle />
--- pokedex/src/hooks/useFetchPokemons.js
+++ pokedex/src/hooks/useFetchPokemons.js
@@ -1,38 +1,72 @@
 import { useEffect, useState } from 'react'
 import axios from 'axios';
 import { URL_BASE } from '../constants/URL_BASE';
+import { replaceText } from '../App';
 
-export const useFechtPokemons = () => {
+export const useFechtPokemons = (path) => {
     const [pokemons, setPokemons] = useState([])
 
     useEffect(() => {
         const fetchData = async () => {
             try {
-                const response = await axios.get(`${URL_BASE}pokemon?offset=0&limit=21`);
-                const urlPokemons = response.data.results.map((item) => { return item.url })
 
-                const listPromises = urlPokemons.map((url) => {
-                    axios.get(url)
-                        .then((response) => {
-                            return response.data
-                        })
-                        .catch((error) => {
-                            console.log(error.response)
-                        })
-                })
+                let listPokemons
 
-                const listPokemons = await Promise.all(listPromises)
-                setPokemons(listPokemons)
+                if(!path){
+
+                    try {
+                        let response = await axios.get(`${URL_BASE}pokemon?offset=0&limit=21`)
+                    response = response.data.results
+                    const urlPokemons = response.map((item) => { return item.url })
+
+                    const listPromises = urlPokemons.map(async (url) => {
+                        try {
+                        const pokemon = await axios(url)
+                        return pokemon.data
+                        } catch (error) {
+                        console.log(error.response)
+                        }
+                    });
+                
+                    listPokemons = await Promise.all(listPromises) 
+                    } catch (error) {
+                       console.log(error.response) 
+                    }
+                   
+
+                }else{
+
+                    try {
+                        listPokemons = [(await axios.get(`${URL_BASE + path}`)).data]
+                    } catch (error) {
+                        console.log(error.response)
+                    }
+                    
+                }              
+
+                setPokemons(listPokemons.map((item) => {
+                    return {
+                      id: item.id,
+                      name: replaceText(item.name, true),
+                      types: item.types.map((itemTypes) => { return itemTypes.type.name }),
+                      imagePokemonDefault: item.sprites.other.dream_world.front_default,
+                      secondImagesPokemons: [item.sprites.front_default, item.sprites.back_default, item.sprites.front_female, item.sprites.back_female],
+                      moves: item.moves.slice(0, 10).map((itemMove) => { return replaceText(itemMove.move.name, true, true) }),
+                      baseStats: item.stats.map((statItem) => {
+                        return { [replaceText(statItem.stat.name)]: statItem.base_stat }
+                      })
+                    }
+                  }))
 
             } catch (error) {
-                console.error(error);
+                console.error(error.response);
             }
         };
 
         fetchData();
-    }, []);
+    }, [path]);
 
-    return pokemons;
+    return {pokemons};
 }
 
 
